ca7f13a0736076a70529dcd21353e4a70af32ae8
Julė Jankauskaitė <j.jankauskaite@adform.com>
2024-01-15 13:27:29 +0200

    initial push

--- ItemStore.WebApi/Scripts/006 shop_items create.sql
+++ ItemStore.WebApi/Scripts/006 shop_items create.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE shop_items (
-	shop_id INT,
-	item_id INT,
-	FOREIGN KEY (shop_id) REFERENCES shops(id),
-	FOREIGN KEY (item_id) REFERENCES items(id)
+	shopid INT,
+	itemid INT,
+	FOREIGN KEY (shopid) REFERENCES shops(id),
+	FOREIGN KEY (itemid) REFERENCES items(id)
 );
